d7efe63f0a624c836749db2f4132ab4d056c5dab
nambarr <146008718+nambarr@users.noreply.github.com>
2024-11-08 06:40:18 +0700

    Create sepo.py

---
+++ sepo.py
@@ -0,0 +1,40 @@
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+options = {
+    'proxy': {
+        'http': 'http://08a81e1e1057435c51a7__cr.sg:3dccd3ecf9cf18cb@gw.dataimpulse.com:10000',
+        'https': 'https://08a81e1e1057435c51a7__cr.sg:3dccd3ecf9cf18cb@gw.dataimpulse.com:10000',
+        'no_proxy': 'localhost,127.0.0.1'
+    }
+}
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--verbose")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=1920, 1200")
+# Hapus '--headless' untuk melihat apakah ekstensi berjalan dengan benar dalam mode normal
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
+
+driver.get('https://httpbin.org/ip')
+print(driver.page_source)
+
+time.sleep(1)
+
+driver.get("https://sepolia-faucet.pk910.de/#/mine/7525a731-a579-4014-8862-73db8ea82717")
+time.sleep(50)
+
+div_element = driver.find_element(By.CLASS_NAME, "col-3")
+content_text = div_element.text
+print(content_text)
+
+#WAKTU MENUNGGU MINING SELESAI
+time.sleep(15000)
+
+# Tutup browser
+driver.quit()
